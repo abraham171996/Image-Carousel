8af28f79deb554a1d240b4c1551548fccf32a04c
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-07-04 20:33:04 +0400

    update app.js

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-
-
 const randomImg = document.querySelector(".swiper-wrapper");
-const Next = document.querySelector(".swiper-button-next")
-const Prev = document.querySelector(".swiper-button-prev")
+const Next = document.querySelector(".swiper-button-next");
+const Prev = document.querySelector(".swiper-button-prev");
 let imgArr = [];
+let imgInterval;
 
 function reloadeCarousel() {
   fetch("https://source.unsplash.com/random/500x500").then((res) => {
     imgArr.push(res.url);
-   
+
     const swiper = new Swiper(".mySwiper", {
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
-     
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
   });
- 
 }
-setTimeout(()=>{
-    Next.style.display = "block"
-    Prev.style.display = "block"
-},3000)
- setInterval(() => {
+
+imgInterval = setInterval(() => {
   if (imgArr.length > 5) {
     imgArr.shift();
   }
@@ -46,15 +40,56 @@ setTimeout(()=>{
       `;
     });
   }
+}, 2000);
 
-  
-  console.log(imgArr);
-}, 1500);
+setTimeout(() => {
+  Next.style.display = "block";
+  Prev.style.display = "block";
+}, 4000);
 
-// Next.addEventListener("click",()=>{
-//     clearInterval(imgIterval)
-// })
-// imgIterval
-// Prev.addEventListener("click",()=>{
-//     clearInterval(imgIterval)
-// })
+Next.addEventListener("click", () => {
+  clearInterval(imgInterval);
+  imgInterval = setInterval(() => {
+    if (imgArr.length > 5) {
+      imgArr.shift();
+    }
+
+    reloadeCarousel();
+
+    if (imgArr.length < 6) {
+      randomImg.innerHTML = "";
+
+      imgArr.forEach((item) => {
+        randomImg.innerHTML += `
+          <div class="swiper-slide">
+            <img src="${item}" alt="">
+          </div>
+        `;
+      });
+    }
+  }, 2000);
+});
+
+Prev.addEventListener("click", () => {
+  clearInterval(imgInterval);
+  imgInterval = setInterval(() => {
+    if (imgArr.length > 5) {
+      imgArr.shift();
+    }
+
+    reloadeCarousel();
+
+    if (imgArr.length < 6) {
+      randomImg.innerHTML = "";
+
+      imgArr.forEach((item) => {
+        randomImg.innerHTML += `
+          <div class="swiper-slide">
+            <img src="${item}" alt="">
+          </div>
+        `;
+      });
+    }
+  }, 2000);
+});
+
